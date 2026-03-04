d3db165e76f883ec4dcc8307a2a834f3e4a3764b
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-04 23:59:22 +0500

    Create sum_two_numbers.cpp

---
+++ sum_two_numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int yigindi;
+
+    cout << "Raqamni kiriting:\n";
+    cin >> a;
+
+    cout << "Yana raqam kiriting:\n";
+    cin >> b;
+
+    yigindi = a + b;  // Two numbers are added
+
+    cout << "Yigindi teng = " << yigindi << "\n";
+
+    return 0;
+}
