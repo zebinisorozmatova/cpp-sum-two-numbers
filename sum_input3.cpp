3ac3d769493e081e71a9f71a7e4432c85bf9af66
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-05 22:55:03 +0500

    Create sum_input3.cpp

---
+++ sum_input3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1;
+    int num2;
+    int total;
+
+    cout << "Input number 1:\n";
+    cin >> num1;
+
+    cout << "Input number 2:\n";
+    cin >> num2;
+
+    total = num1 + num2;
+
+    cout << "Total = " << total << "\n";
+
+    return 0;
+}
