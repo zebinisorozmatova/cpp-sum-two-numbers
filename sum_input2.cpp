232f81530e0a82e37f4f421a9ec69b033615cfb3
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-05 22:55:13 +0500

    Create sum_input2.cpp

---
+++ sum_input2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int result;
+
+    cout << "Enter first number:\n";
+    cin >> a;
+
+    cout << "Enter second number:\n";
+    cin >> b;
+
+    result = a + b;
+
+    cout << "Sum = " << result << endl;
+
+    return 0;
+}
