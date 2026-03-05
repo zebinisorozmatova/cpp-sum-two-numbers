b278c841fa37997a75571b2a08469d2c2fa80687
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-05 22:54:35 +0500

    Create sum_input1.cpp

---
+++ sum_input1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+    int sum;
+
+    cout << "Birinchi raqamni kiriting:\n";
+    cin >> x;
+
+    cout << "Ikkinchi raqamni kiriting:\n";
+    cin >> y;
+
+    sum = x + y;
+
+    cout << "Natija = " << sum << "\n";
+
+    return 0;
+}
